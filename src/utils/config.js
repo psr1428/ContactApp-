759bd7d4cccee3bca258b4d0c4765815ee072277
Prashant Ranpura <prashantranpura15@gmail.com>
2021-07-15 12:00:54 +0530

    initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,21 @@
+//TODO: add firebase configuration and export it
+
+//image configuration
+export const imageConfig = {
+    quality: 0.2,
+    maxWidth: 800,
+    maxHeight: 600,
+    autoRotate: true
+};
+
+export // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDtfbbmlObr9G80f0bb6bAvEgWbhoNTZFI",
+    authDomain: "gitapp-6544b.firebaseapp.com",
+    databaseURL: "https://gitapp-6544b-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "gitapp-6544b",
+    storageBucket: "gitapp-6544b.appspot.com",
+    messagingSenderId: "987583367945",
+    appId: "1:987583367945:web:e38a09aa85fe4cf95421d8",
+    measurementId: "G-8K8H7GNDH0"
+};
